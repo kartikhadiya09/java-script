65e1a3301ab05f9d16f2b0d8b3247bf5b1cc09c3
admin <mr.nobby009@gmail.com>
2025-03-21 22:07:50 +0530

    types

---
+++ basics/data_types.js
@@ -0,0 +1,8 @@
+let name = "Kartik" // string
+let age = 21 // numbar
+let stockValue = 15254521521515 // bigint
+let isLoading = true // bool
+let value = null // standalone value
+let data // undefined
+
+// symbol ==> unique
