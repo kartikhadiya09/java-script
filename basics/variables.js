352c9bd8e13e753fd1498ae7c73a4b56b11b50f9
admin <mr.nobby009@gmail.com>
2025-03-21 21:52:46 +0530

    learn vatiables

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accoountId = 1
+let accountEmail= "email@gmail.com"
+var accountPassword = "12345678"
+userCity = "Surat"
+
+
+console.table([accoountId,accountEmail,accountPassword,userCity])
